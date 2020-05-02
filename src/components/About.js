22aaaebeec772e6204765d66c3c81dfb7db21d5c
Vaibhav Dhoke <dhoke.v@husky.neu.edu>
2020-05-02 01:58:32 -0400

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="images/mypic.jpeg" alt="" />
+               <img className="profile-pic"  src="images/mypic.jpeg" alt="images/mypic.jpeg" />
 
             </div>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ export default class Header extends Component {
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">{resumeData.name}.</h1>
+               <h1 className="responsive-headline">{resumeData.name}</h1>
                <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>{resumeData.roleDescription}
                </h3>
                <hr/>
