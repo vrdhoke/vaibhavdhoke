551e6090da078e0c4559e4407cd2c28b5676b602
Vaibhav Dhoke <dhoke.v@husky.neu.edu>
2020-05-04 20:34:02 -0400

    initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,6 +23,7 @@ export default class Footer extends Component {
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
       </div>
+      Copyright @ 2020 Vaibhav Dhoke.
     </footer>
     );
   }
