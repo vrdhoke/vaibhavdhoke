0c135a2ecbeeadfe6d7affa201ddcfbe5bc52e68
Vaibhav Dhoke <dhoke.v@husky.neu.edu>
2020-05-04 20:10:23 -0400

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="images/mypic.jpeg" alt="images/mypic.jpeg" />
+               <img className="profile-pic"  src="images/mypic.jpeg"/>
 
             </div>
 
