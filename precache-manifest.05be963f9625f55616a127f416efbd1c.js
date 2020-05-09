ff3b1ba81a302f8142e83331c028086b63aacd55
Vaibhav Dhoke <dhoke.v@husky.neu.edu>
2020-05-09 01:40:27 -0400

    Updates

---
+++ precache-manifest.05be963f9625f55616a127f416efbd1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aacd5c93da80b13a2c56",
+    "url": "/static/css/main.11bcb9bc.chunk.css"
+  },
+  {
+    "revision": "aacd5c93da80b13a2c56",
+    "url": "/static/js/main.aacd5c93.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a0507bcb11e27070fcfa0877acdf15fa",
+    "url": "/index.html"
+  }
+];
